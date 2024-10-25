82ee908392ec499cfc5a7db6cd048adfa62bc5e7
rjtkumar <rjtkumardev@gmail.com>
2024-10-26 03:21:50 +0530

    Scrapy Fake User Agents: How to Manage User Agents When Scraping
    https://scrapeops.io/python-scrapy-playbook/scrapy-managing-user-agents/
    
    Defining our own list of fake user agents, and choosing one of them at random
    to make the request with each time the request is made.

--- chocolatescraper/chocolatescraper/spiders/chocolatespider.py
+++ chocolatescraper/chocolatescraper/spiders/chocolatespider.py
@@ -3,6 +3,7 @@ from chocolatescraper.items import ChocolateProduct
 from chocolatescraper.itemloaders import ChocolateProductLoader
 from urllib.parse import urlencode
 from chocolatescraper.config import SCRAPEOPS_API_KEY
+import random
 
 def get_proxy_url (url):
     # This is how to use proxy API's endpoint. The API handles everything for us
@@ -10,6 +11,14 @@ def get_proxy_url (url):
     proxy_url = 'https://proxy.scrapeops.io/v1/?' + urlencode(payload)
     return proxy_url
 
+# We can also rotate between different user agents ourselves when yielding a Request object
+user_agent_list = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36',
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
+    'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18363',
+]
 
 class ChocolatespiderSpider(scrapy.Spider):
     name = "chocolatespider"
@@ -21,7 +30,7 @@ class ChocolatespiderSpider(scrapy.Spider):
             callback= self.parse,
             # We can also set up the user-agent in the scrapy Request object
             headers = {
-                'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64; rv:131.0) Gecko/20100101 Firefox/131.0'
+                "User-Agent" : random.choice(user_agent_list) # Choosing a random user agent to make the request with
             }
         )
 
@@ -46,7 +55,6 @@ class ChocolatespiderSpider(scrapy.Spider):
                 url= next_page_url,
                 callback= self.parse,
                 headers = {
-                    # Setting up a static user agent for the request being made
-                    'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64; rv:131.0) Gecko/20100101 Firefox/131.0'
+                    "User-Agent" : random.choice(user_agent_list) # Choosing a random user agent to make the request with
                 }
             )
