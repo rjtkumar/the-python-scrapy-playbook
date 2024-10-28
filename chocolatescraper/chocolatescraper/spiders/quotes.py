59154deb3c37d9172f946305def8005897274c7f
rjtkumar <rjtkumardev@gmail.com>
2024-10-29 00:31:54 +0530

    Scrapy Playwright Guide: Render & Scrape JS Heavy Websites
    https://scrapeops.io/python-scrapy-playbook/scrapy-playwright/
    
    Taking screenshot of a webpage.

--- chocolatescraper/chocolatescraper/spiders/quotes.py
+++ chocolatescraper/chocolatescraper/spiders/quotes.py
@@ -17,21 +17,13 @@ class QuotesSpider(scrapy.Spider):
             'errback' : self.errback,
             'playwright_page_methods' : [
                 PageMethod('wait_for_selector', 'div.quote'),
-                PageMethod('evaluate', 'window.scrollBy(0, document.body.scrollHeight)'),
-                PageMethod('wait_for_selector', 'div.quote:nth-child(11)'),
             ]
         })
     
     async def parse(self, response):
         page = response.meta['playwright_page']
+        screenshot = await page.screenshot(path= "example.png", full_page = True)
         await page.close()
-
-        for quote in response.css('div.quote'):
-            quote_item = QuoteItem()
-            quote_item['text'] = quote.css('span.text::text').get()
-            quote_item['author'] = quote.css('small.author::text').get()
-            quote_item['tags'] = quote.css('div.tags a.tag::text').getall()
-            yield quote_item
     
     async def errback (self, failure):
         page = failure.request.meta['playwright_page']
