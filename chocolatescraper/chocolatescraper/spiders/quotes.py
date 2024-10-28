420efae990cb9aaa98444dddcccf469838a70ad4
rjtkumar <rjtkumardev@gmail.com>
2024-10-29 00:27:24 +0530

    Scrapy Playwright Guide: Render & Scrape JS Heavy Websites
    https://scrapeops.io/python-scrapy-playbook/scrapy-playwright/
    
    Scraping an infinite scroll page.

--- chocolatescraper/chocolatescraper/spiders/quotes.py
+++ chocolatescraper/chocolatescraper/spiders/quotes.py
@@ -7,7 +7,7 @@ class QuotesSpider(scrapy.Spider):
     name = "quotes"
 
     def start_requests(self):
-        url = 'https://quotes.toscrape.com/js/'
+        url = 'https://quotes.toscrape.com/scroll' # Now scrolling the infinite scroll version of the website
         # We need to explicitly tell scrapy to use playwright for each request
         yield scrapy.Request(url, meta = {
             'playwright' : True,
@@ -16,7 +16,9 @@ class QuotesSpider(scrapy.Spider):
             # When using 'playwirght_include_page' set to True, it is also recommended to  provide an errback to clode the page
             'errback' : self.errback,
             'playwright_page_methods' : [
-                PageMethod('wait_for_selector', 'div.quote')
+                PageMethod('wait_for_selector', 'div.quote'),
+                PageMethod('evaluate', 'window.scrollBy(0, document.body.scrollHeight)'),
+                PageMethod('wait_for_selector', 'div.quote:nth-child(11)'),
             ]
         })
     
@@ -30,21 +32,6 @@ class QuotesSpider(scrapy.Spider):
             quote_item['author'] = quote.css('small.author::text').get()
             quote_item['tags'] = quote.css('div.tags a.tag::text').getall()
             yield quote_item
-        
-        next_page = response.css('.next>a ::attr(href)').get()
-        if next_page is not None:
-            next_page_url = 'http://quotes.toscrape.com' + next_page
-            yield scrapy.Request(
-                next_page_url,
-                meta = dict(
-                    playwright = True,
-                    playwright_include_page = True,
-                    playwright_page_methods = [
-                        PageMethod('wait_for_selector', 'div.quote')
-                    ],
-                    errback = self.errback
-                )
-            )
     
     async def errback (self, failure):
         page = failure.request.meta['playwright_page']
