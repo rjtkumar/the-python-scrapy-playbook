06d442a75779e3f71047386671150169179cbf7c
rjtkumar <rjtkumardev@gmail.com>
2025-04-15 15:13:44 +0530

    Scrapy Pagination Guide: The 6 Most Popular Pagination Methods https://scrapeops.io/python-scrapy-playbook/scrapy-pagination-guide/
    06 Scrapy Pagination Guide, The 6 Most Popular Pagination Methods.pdf
    
    - Request the next page by incrementing the page number until you recieve
      a 404 response or empty page

--- scrapeops/scrapeops/spiders/quotes.py
+++ scrapeops/scrapeops/spiders/quotes.py
@@ -1,18 +1,37 @@
 import scrapy
+from scrapy.exceptions import CloseSpider
 
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
     allowed_domains = ["quotes.toscrape.com"]
 
-    # Generating requests by changing page numbers in the URL for scraping to a fixed page depth or if the end is known
-    start_urls = ['https://quotes.toscrape.com/page/%d/' % i for i in range(1,11)]
-    
+    start_urls = ['https://quotes.toscrape.com/page/1/']
+    # By default scrapy only processes codes 200 - 300, To continue normal execution upon receiving a 404 status code
+    handle_httpsstatus_list = [404]
+    # To keep track of the current page number
+    page_number = 1
 
     def parse(self, response):
+
+        # If the status code is 404 Close the spider with our message
+        if response.status == 404:
+            raise CloseSpider('Recieved %d response code' %response.status)
+        
+        quotes = response.css('div.quote')
+
+        # If there are no quotes on a page, Close the spider
+        if len(quotes) == 0:
+            raise CloseSpider('Recieved no quotes in response')
+        
         for quote in response.css('div.quote'):
             yield {
                 'author' : quote.css('small.author::text').get(),
                 'quote' : quote.css('span.text::text').get(),
                 'tags' : quote.css('a.tag::text').getall(),
             }
+
+        # If page was successfully scraped, update the page number and go to next page
+        self.page_number += 1
+        next_page = f'https://quotes.toscrape.com/page/{self.page_number}/'
+        yield response.follow(next_page, callback = self.parse)
