09f5b722b0c95dd630b8f9e97ecf83200b8f6e39
rjtkumar <rjtkumardev@gmail.com>
2025-04-21 16:41:41 +0530

    Scrapy Pagination Guide: The 6 Most Popular Pagination Methods https://scrapeops.io/python-scrapy-playbook/scrapy-pagination-guide/
    06 Scrapy Pagination Guide, The 6 Most Popular Pagination Methods.pdf
    
    Make the spider raise multiple parallel request and scrape for an API paginated
    source where the total number of pages is available in the initial request and the
    url follows a predictable pattern for pagination.
    Significant time is saved because of parallelization.

--- scrapeops/scrapeops/spiders/rick_and_morty_api.py
+++ scrapeops/scrapeops/spiders/rick_and_morty_api.py
@@ -6,6 +6,10 @@ class RickAndMortyApiSpider(scrapy.Spider):
     allowed_domains = ["rickandmortyapi.com"]
     start_urls = ["https://rickandmortyapi.com/api/character/"]
 
+    custom_settings = {
+        # 'CONCURRENT_REQUESTS' : 1 # To check the difference created by parallel requests
+    }
+
     def parse(self, response):
 
         json_response = response.json() # processes the json encoded content of the response and returns a dict
@@ -18,9 +22,11 @@ class RickAndMortyApiSpider(scrapy.Spider):
                     'gender' : character.get('gender'),
                     'url' : character.get('url'),
                 }
-
-        # If the next page exists, yield follow and parse requests
-        if json_response.get('info'):
-            next_page = json_response['info'].get('next')
-            if next_page:
-                yield response.follow(url= next_page, callback= self.parse)
+        
+        # While on the first page we generate all the request necessary given the total number of pages and a pattern for pagignation
+        if response.url == "https://rickandmortyapi.com/api/character/":
+            if json_response.get('info'):
+                num_pages = json_response['info'].get('pages')
+                for page in range(2,num_pages):
+                    next_page = f'https://rickandmortyapi.com/api/character/?page={page}'
+                    yield response.follow(url= next_page, callback= self.parse)
