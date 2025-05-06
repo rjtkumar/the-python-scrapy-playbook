98714f6a4a84bf6c1ce77e749669af29bed8e8bf
rjtkumar <rjtkumardev@gmail.com>
2025-05-06 05:32:00 +0530

    Scrapy Fake User Agents: How to Manage User Agents When Scraping https://scrapeops.io/python-scrapy-playbook/scrapy-managing-user-agents/
    09 Scrapy Fake User Agents - How to Manage User Agents When Scraping.pdf
    
    Implement ScrapeOpsFakeUserAgentMiddleware which downloads a list of fake User-Agents
    and sends a random User-Agent with every request made.

--- scrapeops/scrapeops/middlewares.py
+++ scrapeops/scrapeops/middlewares.py
@@ -4,7 +4,9 @@
 # https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
 from scrapy import signals
-
+import requests
+import random
+from urllib.parse import urlencode
 # useful for handling different item types with a single interface
 from itemadapter import is_item, ItemAdapter
 
@@ -101,3 +103,78 @@ class ScrapeopsDownloaderMiddleware:
 
     def spider_opened(self, spider):
         spider.logger.info("Spider opened: %s" % spider.name)
+
+# # My solution
+# class RandomUserAgentMiddleware:
+    
+#     fake_user_agents = []
+
+#     @classmethod
+#     def from_crawler (cls, crawler):
+#         s = cls(crawler.settings['SCRAPEOPS_API_KEY'])
+#         crawler.signals.connect(s.spider_opened, signal= signals.spider_opened)
+#         return s
+    
+#     def __init__ (self, SCRAPEOPS_API_KEY):
+#         response = requests.get(
+#             url= 'https://headers.scrapeops.io/v1/user-agents',
+#             params= {
+#                 'api_key' : SCRAPEOPS_API_KEY,
+#                 'num_results' : 10
+#             }
+#         )
+#         self.fake_user_agents = response.json()['result']
+#         print("New User-Agents downloaded: ", self.fake_user_agents)
+
+
+#     def spider_opened (self, spider):
+#         spider.logger.info("RandomUserAgentMiddleware: ready")
+    
+#     def process_request (self, request, spider):
+#         request.headers['User-Agent'] = random.choice(self.fake_user_agents)
+#         print(f"\n\nNew request being generated with middleware: {request.headers['User-Agent']}")
+#         return None
+
+class ScrapeOpsFakeUserAgentMiddleware:
+
+    @classmethod
+    def from_crawler (cls, crawler):
+        return cls(crawler.settings)
+    
+    def __init__ (self, settings):
+        self.scrapeops_api_key = settings.get('SCRAPEOPS_API_KEY')
+        self.scrapeops_endpoint = settings.get('SCRAPEOPS_FAKE_USER_AGENT_ENDPOINT', 'https://headers.scrapeops.io/v1/user-agents?')
+        self.scrapeops_fake_user_agents_active = settings.get('SCRAPEOPS_FAKE_USER_AGENT_ENABLED', False)
+        self.scrapeops_num_results = settings.get('SCRAPEOPS_NUM_RESULTS')
+        self.header_list = []
+        self._get_user_agent_list()
+        self._scrapeops_fake_user_agents_enabled()
+
+    def _get_user_agent_list (self):
+        payload = {'api_key' : self.scrapeops_api_key}
+        if self.scrapeops_num_results is not None:
+            payload['num_results'] = self.scrapeops_num_results
+        response = requests.get(
+            self.scrapeops_endpoint,
+            params = urlencode(payload)
+        )
+        print(response.text)
+        json_response = response.json()
+        self.user_agent_list = json_response.get('result')
+    
+    def _get_random_user_agent (self):
+        random_index = random.randint(0, len(self.user_agent_list)-1)
+        return self.user_agent_list[random_index]
+    
+    def _scrapeops_fake_user_agents_enabled (self):
+        if self.scrapeops_fake_user_agents_active == False or \
+            self.scrapeops_api_key == '' or\
+            self.scrapeops_api_key is None :
+            self.scrapeops_fake_user_agents_active = False
+        else:
+            self.scrapeops_fake_user_agents_active = True
+    
+    def process_request (self, request, spider):
+        random_user_agent = self._get_random_user_agent()
+        # print(random_user_agent)
+        request.headers['User-Agent'] = random_user_agent
--- scrapeops/scrapeops/spiders/quotes.py
+++ scrapeops/scrapeops/spiders/quotes.py
@@ -2,33 +2,48 @@ import scrapy
 from scrapy.exceptions import CloseSpider
 from scrapeops.items import QuoteItem
 import random
+from scrapeops.secrets import SCRAPEOPS_API_KEY
 
-user_agent_list = [ # User-Agent list for us to choose randomly from
-'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36',
-'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
-'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
-'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75',
-'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18363',
-]
+# user_agent_list = [ # User-Agent list for us to choose randomly from
+# 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36',
+# 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
+# 'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
+# 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75',
+# 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18363',
+# ]
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
     allowed_domains = ["quotes.toscrape.com"]
 
+    custom_settings = {
+        # # for scrapeops.middlewares.RandomUserAgentMiddleware
+        # 'DOWNLOADER_MIDDLEWARES' : {
+        #     'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware' : 350,
+        #     'scrapeops.middlewares.RandomUserAgentMiddleware' : 400,
+        # },
+        'SCRAPEOPS_API_KEY' : SCRAPEOPS_API_KEY,
+        # for scrapeops.middlewares.ScrapeOpsFakeUserAgentMiddleware 
+        'SCRAPEOPS_FAKE_USER_AGENT_ENABLED' : True,
+        'DOWNLOADER_MIDDLEWARES' : {
+            'scrapeops.middlewares.ScrapeOpsFakeUserAgentMiddleware': 400,
+        }
+    }
+
     def start_requests(self):
         yield scrapy.Request(
             url= 'https://quotes.toscrape.com',
             callback= self.parse,
-            # Sending a random User-Agent with every request made 
-            headers= {
-                'User-Agent' : random.choice(user_agent_list)
-            }
+            # # Sending a random User-Agent with every request made 
+            # headers= {
+            #     'User-Agent' : random.choice(user_agent_list)
+            # }
         )
 
     def parse(self, response):
 
         # Dictionaries, dataclasses and attrs objects are converted to scrapy.Item automatically upon yielding
-        print(response.request.headers)
+        # print(response.request.headers) # For checking User-Agent
         quote_item = QuoteItem()
         for quote in response.css('div.quote'):
             quote_item['text'] = quote.css('span.text::text').get()
@@ -43,7 +58,7 @@ class QuotesSpider(scrapy.Spider):
             yield response.follow(
                 url= next_page,
                 callback = self.parse,
-                headers = { # Sending random User-Agent with every request made
-                    'User-Agent' : random.choice(user_agent_list)
-                }
+                # headers = { # Sending random User-Agent with every request made
+                #     'User-Agent' : random.choice(user_agent_list)
+                # }
             )
