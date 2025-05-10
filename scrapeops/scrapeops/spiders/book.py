8738fa2327d1d749ca8f666c4e9dcdd67c491101
rjtkumar <rjtkumardev@gmail.com>
2025-05-11 04:15:53 +0530

    Saving Scraped Data To JSON With Scrapy Feed Exporters https://scrapeops.io/python-scrapy-playbook/scrapy-save-json-files/
    12 Scrapy Database Guide - Saving Data To JSON Files.pdf
    
    - Store output to a JSON & JOSN-lines file via the command line
    - Save output JSON & JSON-lines file via FEEDS setting
    - Dynamic file names and other options available via the FEEDS setting
    - Saving data to multiple JSON-lines file batches

--- scrapeops/scrapeops/spiders/book.py
+++ scrapeops/scrapeops/spiders/book.py
@@ -1,5 +1,5 @@
 import scrapy
-
+import pdb
 
 class BookSpider(scrapy.Spider):
     name = "books"
@@ -7,41 +7,36 @@ class BookSpider(scrapy.Spider):
     start_urls = ["https://books.toscrape.com/"]
 
     custom_settings = {
-        # Saving files with FEEDS settings
+        # Saving to JSON and JOSN lines via FEEDS setting
         'FEEDS' : {
-            # Static file name
-            './scrapy_output/data.csv' : {
-                'format' : 'csv',
-                'overwrite' : True,# When saving locally overwrite is False by default
-            },
+            './scrapy_output/data.json' : {'format':'json'}, # JSON
+            './scrapy_output/data2.jsonl' : {'format':'jsonlines', 'overwrite': False}, # JSON lines (Recommended for scraping large data)
+            # Oerwriting behaviour of FEEDS depends on where data is going to be saved, saving locally 'overwrite' is False
 
-            # Dynamic file name
-            # Any named parameter gets replaced by the spider attribute of the same name
-            # %(site_id)s would get replaced by spider.side_id
-            './scrapy_output/data/%(name)s/%(name)s_%(time)s.csv' : { # ./data/spidername/spidername_executiontime.csv
-                'format' : 'csv',
+            # Dynamic filepath, Any named parameter would get replalced by the spider attribute of the same name
+            # %(site_id)s would get replaced with spider.site_id
+            './scrapy_output/%(name)s/%(name)s_%(time)s.jsonl' : {
+                'format' : 'jsonlines',
+                'overwrite' : True, # Overwrite existing file?
                 'encoding' : 'utf-8',
-                'store_empty' : False,
-                'fields' : ['name', 'price'],
-                'indent' : 4,
-                'overwrite' : False,
-                # Other FEEDS dunctionality we can define
-                # encoding (default utf-8 except for json)
-                # fields : A list of fields to export, only save certain fields
-                # item_classes : A list of item classes to export
-                # item_filter : A filter class to filter items (ItemFilter is used by default)
-                # indent : Amount of spaces to indent output on each level
-                # store_empty : Whether to export empty feeds
-                # uri_params : A string with import path of a function to set the parameters to apply with printf style string formatting to the feed uri
-                # postprocessing : List of plugins to use for post-processing
+                'store_empty' : True, # Whether to export empty feeds?
+                'fields' : ['name', 'price', ], # Fields to export
+                'indent' : 4, # Amount of spaces used to indent output on each level
+                'item_export kwargs' : {            # pdb.set_trace()
+                    'export_empty_fields' : True,
+                },
+                # Other exporting options
+                # item_filter : filter class to filter items to export
+                # uri_params : A string with the import path of a function to set the parameters to apply with printf-style string formatting to the feed URI.
+                # postprocessing : List of plugigns to use for post-processing
             },
 
-            # Breaking up a spider output file into batches
-            # When storing in batches it becomes necessary to use batch_it or batch_time placeholders in output file URI
-            # to indicate how different output file will be generated and named
-            './scrapy_output/batch_data/%(name)s_%(batch_id)s.csv' : {
-                'format' : 'csv',
-                'batch_item_count': 10
+            # Saving data to multiple JSON file batches to make the data more managable using batch sizes 'batch_item_count'
+            # When we specify a batch size we also need to use either 'batch_id' or 'batch_time' into the file path
+            './scrapy_output/%(name)s/batch_%(batch_id)s.jsonl' : {
+                'format' : 'jsonlines',
+                # 'batch_item_count' specifies the size of 1 file/ batch
+                'batch_item_count' : 10 # Starts recording data into a new file every 10 items
             }
         }
     }
